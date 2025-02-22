29703ab8dfa5c3300da332e791a3fd8a8cb701e7
BaekSukSu <92211957+BaekSukSu@users.noreply.github.com>
2025-02-22 11:31:58 +0900

    [Silver III] Title: 파일 정리, Time: 252 ms, Memory: 13776 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -5,24 +5,24 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    
+
     map<string, int> ext_count;
-    
-    while (n--) {
+
+    for(int i = 0; i < n; i++) {
         string filename;
         cin >> filename;
-        
+
         // 마지막 '.' 이후가 확장자
-        size_t dot_pos = filename.find_last_of('.');
-        string ext = filename.substr(dot_pos + 1);
-        
+        int dot = filename.find_last_of('.');
+        string ext = filename.substr(dot + 1);
+
         ext_count[ext]++;
     }
-    
+
     // 자동 정렬된 확장자 출력
     for (const auto& pair : ext_count) {
         cout << pair.first << " " << pair.second << "\n";
     }
-    
+
     return 0;
 }
