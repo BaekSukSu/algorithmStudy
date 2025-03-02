bb4df1b6533f91f1ba88863d59a530ebf6947ab6
BaekSukSu <92211957+BaekSukSu@users.noreply.github.com>
2025-03-02 12:19:18 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.cc
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -3,38 +3,35 @@
 using namespace std;
 
 int main() {
-    int n, w, L;
-    cin >> n >> w >> L;
+    int n, length, weight;
+    cin >> n >> length >> weight;
 
-    queue<int> trucks;
-    for (int i = 0; i < n; i++) {
-        int truck;
-        cin >> truck;
-        trucks.push(truck);
+    queue <int> truck;
+    for(int i=0; i<n; i++) {
+        int w;
+        cin >> w;
+        truck.push(w);
     }
 
-    queue<int> bridge; // 현재 다리 위의 트럭
-    int bridge_weight = 0, time = 0;
-
-    // 다리의 길이만큼 0으로 초기화 (트럭이 없는 상태)
-    for (int i = 0; i < w; i++) {
+    queue <int> bridge;
+    for(int i=0; i<length; i++) {
         bridge.push(0);
     }
 
-    while (!trucks.empty() || bridge_weight > 0) {
+    int totalWeight, time=0;
+
+    while(!truck.empty() || totalWeight > 0) {
         time++;
 
-        // 다리에서 맨 앞 트럭 제거
-        bridge_weight -= bridge.front();
+        totalWeight -= bridge.front();
         bridge.pop();
 
-        // 새로운 트럭을 다리에 올릴 수 있는지 확인
-        if (!trucks.empty() && bridge_weight + trucks.front() <= L) {
-            bridge_weight += trucks.front();
-            bridge.push(trucks.front());
-            trucks.pop();
+        if(!truck.empty() && totalWeight + truck.front() <= weight) {
+            totalWeight += truck.front();
+            bridge.push(truck.front());
+            truck.pop();
         } else {
-            bridge.push(0); // 트럭이 못 올라가면 빈 공간(0)을 추가
+            bridge.push(0);
         }
     }
 
